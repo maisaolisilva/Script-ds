04d5d57e1abf3883e915f1c57cbef03438c6b915
Maisa Oliveira <maisaolisilva@gmail.com>
2025-02-13 15:30:57 -0300

    primeiro commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,99 @@
+"use client";
+
 import Image from "next/image";
+import styled from "styled-components";
 
-export default function Home() {
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  header{
+    margin-top: 15px;
+  }
+  .linha{
+    background: #15f5ba;
+    height: 1px;
+    margin-top: 10px;
+  }
+  main{
+    h1{
+      text-align: center;
+    }
+    .container{
+      background: #2B2B2B;
+
+      .introducao{
+        display: flex;
+        flex-direction: column;
+        align-items: center; 
+        justify-content: center; 
+        text-align: justify; 
+        margin: 0 20px;
+      }
+    }
+    .servicos{
+      .conteudo{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 20px;
+        gap: 38px;
+        h2{
+          font-size: 20px;
+        }
+      }
+      ul{
+        margin: 10px 20px;
+        li{
+          color: #FFFFFF;
+          strong{
+            color: #15f5ba;
+          }
+        }
+      }
+    }
+
+  }
+`
+
+export default function Page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <StyledDiv>
+      <header>
+        <Image src="/script.svg"
+        alt="Logo da empresa"
+          width={300}
+          height={300} />
+      </header>
+     
+      <main>
+        <h1><div>TEM UMA IDEIA?</div> EU DESENVOLVO!</h1>
+        <div className="linha"></div>
+        <div className="container">
+          <div className="introducao">
+            <p>Desenvolvo sites personalizados para empresas, empreendedores e criadores de conteúdo. </p>
+            <p>Soluções web sob medida para o seu negócio e que cabem no seu bolso.</p>
+          </div>
         </div>
+        <section className="servicos">
+          <div className="conteudo">
+            <h2>Serviços oferecidos:</h2>
+            <Image src="/www.svg"
+            alt="imagem com uma tela de computador sendo apontada por um cursor"
+            width={136}
+            height={136} />
+          </div>
+          <ul>
+            <li><strong>Institucionais</strong> → Apresenta a tua empresa de forma profissional</li>
+            <li><strong>Lojas Virtuais</strong> → Vende online com um e-commerce eficiente</li>
+            <li><strong>Blogs & Portais</strong> → Constrói a tua autoridade com um blog otimizado</li>
+            <li><strong>Portfólios</strong> → Mostra o teu trabalho de forma impactante</li>
+            <li><strong>Landing Pages & Hotsites</strong> → Páginas estratégicas para conversão</li>
+          </ul>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+    </StyledDiv>
   );
 }
