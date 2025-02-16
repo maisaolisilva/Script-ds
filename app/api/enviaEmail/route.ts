a4bd4c3fd7b404aecaf3128de0c03cfc35799a16
Maisa Oliveira <maisaolisilva@gmail.com>
2025-02-16 10:58:02 -0300

    notificação enviada por email

--- app/api/enviaEmail/route.ts
+++ app/api/enviaEmail/route.ts
@@ -13,7 +13,7 @@ export async function POST(req: Request) {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT),
-      secure: false, // Usa STARTTLS
+      secure: true, // Usa STARTTLS
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
