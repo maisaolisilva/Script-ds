f769ace7438664b2390b036594e727e79ec96189
Maisa Oliveira <maisaolisilva@gmail.com>
2025-02-19 16:39:09 -0300

    cliente passou a receber um e-mail de confirmação do cadastro

--- app/api/enviaEmail/route.ts
+++ app/api/enviaEmail/route.ts
@@ -31,6 +31,32 @@ export async function POST(req: Request) {
       `,
     });
 
+    //Envia e-mail para o cliente.
+    const clienteEmail = await resend.emails.send({
+      from: `no-reply@teudominio.com`, // Usa o teu domínio verificado no Resend
+      to: body.email, // Email do cliente cadastrado
+      subject: "Confirmação de Pedido de Orçamento ✅",
+      text: `Olá ${body.nome}, recebemos sua solicitação de orçamento! Logo entraremos em contato.`,
+      html: `
+        <h2 style="color: #333;">Olá ${body.nome},</h2>
+        <p>Obrigado por entrar em contato! Recebemos a sua solicitação de orçamento e, em breve, entrarei em contacto para entender melhor as suas necessidades e apresentar a melhor solução para o seu site.</p>
+        <p>Enquanto aguarda o meu retorno, pode já começar a pensar na estrutura e funcionalidades do seu site. Aqui estão algumas perguntas que podem ajudar nesse processo:</p>
+        <ul style="list-style-type: disc; padding-left: 20px;">
+          <li>Qual é o principal objetivo do seu site? (Ex.: vender produtos, divulgar serviços, captar contactos, blog, etc.)</li>
+          <li>Tem alguma referência de site que gosta e gostaria de seguir como inspiração?</li>
+          <li>Quais páginas e seções considera essenciais? (Ex.: Página inicial, Sobre, Serviços, Portefólio, Contactos, etc.)</li>
+          <li>Já tem identidade visual (logotipo, cores, tipografia) ou precisa de ajuda com isso?</li>
+          <li>Precisa de funcionalidades específicas? (Ex.: formulário de contacto, integração com redes sociais, loja online, área de membros, etc.)</li>
+        </ul>
+        <p>Com estas informações, poderei desenvolver uma proposta mais alinhada com as suas expectativas.</p>
+        <p>Caso tenha alguma dúvida, pode entrar em contacto comigo a qualquer momento pelo e-mail scriptdesenvolvimento@gmail.com ou pelo WhatsApp +55 24 99258-1089.</p>
+        <p>Em breve falaremos mais sobre o seu projeto!</p>
+        <p>Cumprimentos,</p>
+        <p>Maisa</p>
+        <p>SCRIPT - Desenvolvimento de Sites Ltda</p>
+              `,
+    });
+
     console.log("✅ Email enviado com sucesso!", response);
     return NextResponse.json({ message: "Email enviado com sucesso!", response }, { status: 200 });
 
