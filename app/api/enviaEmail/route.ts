cc1ba713bf1686de921f88e401eb3999c227839c
Maisa Oliveira <maisaolisilva@gmail.com>
2025-02-25 13:39:26 -0300

    correção na chamada da api de enviar email no formulário

--- app/api/enviaEmail/route.ts
+++ app/api/enviaEmail/route.ts
@@ -39,17 +39,17 @@ export async function POST(req: Request) {
       text: `Olá ${body.nome}, recebemos sua solicitação de orçamento! Logo entraremos em contato.`,
       html: `
         <h2 style="color: #333;">Olá ${body.nome},</h2>
-        <p>Obrigado por entrar em contato! Recebemos a sua solicitação de orçamento e, em breve, entrarei em contacto para entender melhor as suas necessidades e apresentar a melhor solução para o seu site.</p>
+        <p>Obrigado por entrar em contato! Recebemos a sua solicitação de orçamento e, em breve, entrarei em contato para entender melhor as suas necessidades e apresentar a melhor solução para o seu site.</p>
         <p>Enquanto aguarda o meu retorno, pode já começar a pensar na estrutura e funcionalidades do seu site. Aqui estão algumas perguntas que podem ajudar nesse processo:</p>
         <ul style="list-style-type: disc; padding-left: 20px;">
-          <li>Qual é o principal objetivo do seu site? (Ex.: vender produtos, divulgar serviços, captar contactos, blog, etc.)</li>
+          <li>Qual é o principal objetivo do seu site? (Ex.: vender produtos, divulgar serviços, captar contatos, blog, etc.)</li>
           <li>Tem alguma referência de site que gosta e gostaria de seguir como inspiração?</li>
-          <li>Quais páginas e seções considera essenciais? (Ex.: Página inicial, Sobre, Serviços, Portefólio, Contactos, etc.)</li>
+          <li>Quais páginas e seções considera essenciais? (Ex.: Página inicial, Sobre, Serviços, Portifólio, Contactos, etc.)</li>
           <li>Já tem identidade visual (logotipo, cores, tipografia) ou precisa de ajuda com isso?</li>
-          <li>Precisa de funcionalidades específicas? (Ex.: formulário de contacto, integração com redes sociais, loja online, área de membros, etc.)</li>
+          <li>Precisa de funcionalidades específicas? (Ex.: formulário de contato, integração com redes sociais, loja online, área de membros, etc.)</li>
         </ul>
         <p>Com estas informações, poderei desenvolver uma proposta mais alinhada com as suas expectativas.</p>
-        <p>Caso tenha alguma dúvida, pode entrar em contacto comigo a qualquer momento pelo e-mail scriptdesenvolvimento@gmail.com ou pelo WhatsApp +55 24 99258-1089.</p>
+        <p>Caso tenha alguma dúvida, pode entrar em contato comigo a qualquer momento pelo e-mail scriptdesenvolvimento@gmail.com ou pelo WhatsApp +55 24 99258-1089.</p>
         <p>Em breve falaremos mais sobre o seu projeto!</p>
         <p>Cumprimentos,</p>
         <p>Maisa</p>
